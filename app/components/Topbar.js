bfde56e98c88053851d73e78d356d1f958167079
aushamim <au.shamim0@gmail.com>
2024-03-16 16:42:48 +0600

    [Add] Sidebar

--- app/components/Topbar.js
+++ app/components/Topbar.js
@@ -28,9 +28,9 @@ const Topbar = () => {
                 <path
                   d="M19 19L13 13L19 19ZM15 8C15 8.91925 14.8189 9.82951 14.4672 10.6788C14.1154 11.5281 13.5998 12.2997 12.9497 12.9497C12.2997 13.5998 11.5281 14.1154 10.6788 14.4672C9.82951 14.8189 8.91925 15 8 15C7.08075 15 6.1705 14.8189 5.32122 14.4672C4.47194 14.1154 3.70026 13.5998 3.05025 12.9497C2.40024 12.2997 1.88463 11.5281 1.53284 10.6788C1.18106 9.82951 1 8.91925 1 8C1 6.14348 1.7375 4.36301 3.05025 3.05025C4.36301 1.7375 6.14348 1 8 1C9.85652 1 11.637 1.7375 12.9497 3.05025C14.2625 4.36301 15 6.14348 15 8Z"
                   stroke="#868686"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
               </svg>
             </button>
@@ -38,7 +38,7 @@ const Topbar = () => {
         </div>
       </div>
 
-      <div className="flex items-center justify-end">
+      <div className="flex items-center justify-end mr-5">
         <div className="dropdown dropdown-bottom dropdown-end">
           <div tabIndex={0} role="button" className="flex items-center gap-2">
             <Image
@@ -149,6 +149,21 @@ const Topbar = () => {
               </a>
             </li>
           </ul>
+        </div>
+
+        <div className="flex items-center ml-5 2xl:hidden">
+          <label
+            htmlFor="drawer-sidebar"
+            className="drawer-button cursor-pointer"
+          >
+            <Image
+              src="/icons/settings.svg"
+              alt=""
+              width={45}
+              height={45}
+              className="w-5"
+            />
+          </label>
         </div>
       </div>
     </div>
--- app/page.js
+++ app/page.js
@@ -1,13 +1,12 @@
-"use client";
-
 import { Bounce, ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Nav from "./components/Nav";
 import Topbar from "./components/Topbar";
+import Sidebar from "./components/Sidebar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="overflow-hidden">
       <div className="p-10 min-h-screen grid grid-cols-[6%_1fr] gap-8">
         <nav>
           <Nav />
@@ -15,10 +14,11 @@ export default function Home() {
 
         <div>
           <Topbar />
-
           <div className="grid grid-cols-[1fr_17%] gap-8">
             <div className="bg-green-200">body</div>
-            <div className="bg-red-200">sidebar</div>
+            <div>
+              <Sidebar />
+            </div>
           </div>
         </div>
       </div>
